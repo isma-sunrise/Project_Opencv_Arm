2bbc84543655583f4d187cb378c5d82978abdc79
ismail bouhout <ismail.bouhout@smile.fr>
2024-03-12 11:03:13 +0100

    show image works / update .gitignore

--- main.cpp
+++ main.cpp
@@ -8,9 +8,17 @@
 int main(int argc, char** argv )
 {
     // read image
-    cv::Mat  image_src = cv::imread( "oldcar.jpg" , cv::IMREAD_COLOR );
-
+    cv::Mat  image_src = cv::imread( "../oldcar.jpg" , cv::IMREAD_COLOR );
     // show image
+        /* SHOW IMAGES */
+    if ( !image_src.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
     cv::imshow("image_src", image_src);
+
+    cv::waitKey();
+    cv::destroyAllWindows();
     return 0;
 }
