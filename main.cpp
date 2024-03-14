9bcf6670ed204e4dcb375f3fa5711abd198bf743
isma <ismail.bouhout@smile.fr>
2024-03-14 16:57:24 +0100

    test rasp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <arm_neon.h>
 
 // Precise Time Measurement
 #if defined(_WIN32) || defined(__CYGWIN__)
@@ -210,6 +211,25 @@ void SobelNonSimd(Rect cropSize, Mat inputImage)
     resultImageNonSimd = outputImage.clone();
 }
 
+void print128_num(int16x8_t var)
+{
+    uint16_t val[8];
+    memcpy(val, &var, sizeof(val));
+    printf("Numerical: %X %X %X %X %X %X %X %X \n", 
+           val[0], val[1], val[2], val[3], val[4], val[5], 
+           val[6], val[7]);
+}
+
+void print128_num(int16x8x2_t var)
+{
+    uint16_t val[8];
+    memcpy(val, &var, sizeof(val));
+    printf("Numerical: %X %X %X %X %X %X %X %X \n %X %X %X %X %X %X %X %X \n", 
+           val[0], val[1], val[2], val[3], val[4], val[5], 
+           val[6], val[7], val[8], val[9], val[10], val[11], val[12], val[13], 
+           val[14], val[15]);
+}
+
 void SobelSimd(Rect cropSize, Mat inputImage)
 {
     Mat outputImage;
@@ -217,8 +237,8 @@ void SobelSimd(Rect cropSize, Mat inputImage)
     // inputImage = imread("boat.png", IMREAD_GRAYSCALE);
     outputImage.create(inputImage.size(), inputImage.depth());
 
-    __m128i p1, p2, p3, p4, p5, p6, p7, p8, p9;
-    __m128i gx, gy, temp, G;
+    int16x8_t p1, p2, p3, p4, p5, p6, p7, p8, p9;
+    int16x8_t gx, gy, temp, G;
 
     outputPointer = outputImage.ptr<uchar>();
     inputPointer = inputImage.ptr<uchar>();
@@ -250,17 +270,25 @@ void SobelSimd(Rect cropSize, Mat inputImage)
             Load 128-bits of integer data from memory into dst. mem_addr does not need to be aligned on any particular boundary.
             */
 
-            p1 = _mm_loadu_si128((__m128i *)(inputPointer + i * width + j));
-            p2 = _mm_loadu_si128((__m128i *)(inputPointer + i * width + j + 1));
-            p3 = _mm_loadu_si128((__m128i *)(inputPointer + i * width + j + 2));
+            p1 = vld1q_s16((const int16_t*)(inputPointer + i * width + j));
+            p2 = vld1q_s16((const int16_t*)(inputPointer + i * width + j + 1));
+            p3 = vld1q_s16((const int16_t*)(inputPointer + i * width + j + 2));
 
-            p4 = _mm_loadu_si128((__m128i *)(inputPointer + (i + 1) * width + j));
-            p5 = _mm_loadu_si128((__m128i *)(inputPointer + (i + 1) * width + j + 1));
-            p6 = _mm_loadu_si128((__m128i *)(inputPointer + (i + 1) * width + j + 2));
+            p4 = vld1q_s16((const int16_t*)(inputPointer + (i + 1) * width + j));
+            p5 = vld1q_s16((const int16_t*)(inputPointer + (i + 1) * width + j + 1));
+            p6 = vld1q_s16((const int16_t*)(inputPointer + (i + 1) * width + j + 2));
 
-            p7 = _mm_loadu_si128((__m128i *)(inputPointer + (i + 2) * width + j));
-            p8 = _mm_loadu_si128((__m128i *)(inputPointer + (i + 2) * width + j + 1));
-            p9 = _mm_loadu_si128((__m128i *)(inputPointer + (i + 2) * width + j + 2));
+            p7 = vld1q_s16((const int16_t*)(inputPointer + (i + 2) * width + j));
+            p8 = vld1q_s16((const int16_t*)(inputPointer + (i + 2) * width + j + 1));
+            p9 = vld1q_s16((const int16_t*)(inputPointer + (i + 2) * width + j + 2));
+
+            if(i == (border - 1) && j == (border - 1)) {
+                std::cout << "step 1 : ";
+                print128_num(p1);
+                std::cout << "step 2 : ";
+                print128_num(vtrnq_s16(p1, p1));
+                std::cout << "step 3 : ";
+            }
 
             /*
             __m128i _mm_srli_epi16 (__m128i a, int imm8)
@@ -271,15 +299,17 @@ void SobelSimd(Rect cropSize, Mat inputImage)
             */
 
             // convert image 8-bit unsigned integer data to 16-bit signed integers to use in arithmetic operations
-            p1 = _mm_srli_epi16(_mm_unpacklo_epi8(p1, p1), 8);
-            p2 = _mm_srli_epi16(_mm_unpacklo_epi8(p2, p2), 8);
-            p3 = _mm_srli_epi16(_mm_unpacklo_epi8(p3, p3), 8);
-            p4 = _mm_srli_epi16(_mm_unpacklo_epi8(p4, p4), 8);
-            p5 = _mm_srli_epi16(_mm_unpacklo_epi8(p5, p5), 8);
-            p6 = _mm_srli_epi16(_mm_unpacklo_epi8(p6, p6), 8);
-            p7 = _mm_srli_epi16(_mm_unpacklo_epi8(p7, p7), 8);
-            p8 = _mm_srli_epi16(_mm_unpacklo_epi8(p8, p8), 8);
-            p9 = _mm_srli_epi16(_mm_unpacklo_epi8(p9, p9), 8);
+
+                        // vshr_n_s16
+            // p1 = _mm_srli_epi16(_mm_unpacklo_epi8(p1, p1), 8);
+            // p2 = _mm_srli_epi16(_mm_unpacklo_epi8(p2, p2), 8);
+            // p3 = _mm_srli_epi16(_mm_unpacklo_epi8(p3, p3), 8);
+            // p4 = _mm_srli_epi16(_mm_unpacklo_epi8(p4, p4), 8);
+            // p5 = _mm_srli_epi16(_mm_unpacklo_epi8(p5, p5), 8);
+            // p6 = _mm_srli_epi16(_mm_unpacklo_epi8(p6, p6), 8);
+            // p7 = _mm_srli_epi16(_mm_unpacklo_epi8(p7, p7), 8);
+            // p8 = _mm_srli_epi16(_mm_unpacklo_epi8(p8, p8), 8);
+            // p9 = _mm_srli_epi16(_mm_unpacklo_epi8(p9, p9), 8);
 
             /*
             __m128i _mm_add_epi16 (__m128i a, __m128i b)
@@ -290,60 +320,60 @@ void SobelSimd(Rect cropSize, Mat inputImage)
             */
 
             // Calculating Gx = (p3 + 2 * p6 + p9) - (p1 + 2 * p4 + p7)
-            gx = _mm_add_epi16(p6, p6);   // 2*p6
-            gx = _mm_add_epi16(gx, p3);   // p3 + 2*p6
-            gx = _mm_add_epi16(gx, p9);   // p3 + 2*p6 + p9
-            gx = _mm_sub_epi16(gx, p1);   // p3 + 2*p6 + p9 - p1
-            temp = _mm_add_epi16(p4, p4); // 2*p4
-            gx = _mm_sub_epi16(gx, temp); // p3 + 2*p6 + p9 - (p1 + 2*p4)
-            gx = _mm_sub_epi16(gx, p7);   // p3 + 2*p6 + p9 - (p1 + 2*p4 + p7)
+            // gx = _mm_add_epi16(p6, p6);   // 2*p6
+            // gx = _mm_add_epi16(gx, p3);   // p3 + 2*p6
+            // gx = _mm_add_epi16(gx, p9);   // p3 + 2*p6 + p9
+            // gx = _mm_sub_epi16(gx, p1);   // p3 + 2*p6 + p9 - p1
+            // temp = _mm_add_epi16(p4, p4); // 2*p4
+            // gx = _mm_sub_epi16(gx, temp); // p3 + 2*p6 + p9 - (p1 + 2*p4)
+            // gx = _mm_sub_epi16(gx, p7);   // p3 + 2*p6 + p9 - (p1 + 2*p4 + p7)
 
             // Calculating Gy = (p1 + 2 * p2 + p3) - (p7 + 2 * p8 + p9)
-            gy = _mm_add_epi16(p2, p2);   // 2*p2
-            gy = _mm_add_epi16(gy, p1);   // p1 + 2*p2
-            gy = _mm_add_epi16(gy, p3);   // p1 + 2*p2 + p3
-            gy = _mm_sub_epi16(gy, p7);   // p1 + 2*p2 + p3 - p7
-            temp = _mm_add_epi16(p8, p8); // 2*p8
-            gy = _mm_sub_epi16(gy, temp); // p1 + 2*p2 + p3 - (p7 + 2*p8)
-            gy = _mm_sub_epi16(gy, p9);   // p1 + 2*p2 + p3 - (p7 + 2*p8 + p9)
+            // gy = _mm_add_epi16(p2, p2);   // 2*p2
+            // gy = _mm_add_epi16(gy, p1);   // p1 + 2*p2
+            // gy = _mm_add_epi16(gy, p3);   // p1 + 2*p2 + p3
+            // gy = _mm_sub_epi16(gy, p7);   // p1 + 2*p2 + p3 - p7
+            // temp = _mm_add_epi16(p8, p8); // 2*p8
+            // gy = _mm_sub_epi16(gy, temp); // p1 + 2*p2 + p3 - (p7 + 2*p8)
+            // gy = _mm_sub_epi16(gy, p9);   // p1 + 2*p2 + p3 - (p7 + 2*p8 + p9)
 
             /*
             __m128i _mm_abs_epi16 (__m128i a)
             Compute the absolute value of packed 16-bit integers in a, and store the unsigned Gs in dst.
             */
 
-            gx = _mm_abs_epi16(gx); // |Gx|
-            gy = _mm_abs_epi16(gy); // |Gy|
+            // gx = _mm_abs_epi16(gx); // |Gx|
+            // gy = _mm_abs_epi16(gy); // |Gy|
 
             // G = |Gx| + |Gy|
-            G = _mm_add_epi16(gx, gy);
+            // G = _mm_add_epi16(gx, gy);
 
             /*
             __m128i _mm_packus_epi16 (__m128i a, __m128i b)
             Convert packed 16-bit integers from a and b to packed 8-bit integers using unsigned saturation, and store the results in dst.
             */
-            G = _mm_packus_epi16(G, G);
+            // G = _mm_packus_epi16(G, G);
 
             /*
             void _mm_storeu_si128 (__m128i* mem_addr, __m128i a)
             Store 128-bits of integer data from a into memory. mem_addr does not need to be aligned on any particular boundary.
             */
-            _mm_storeu_si128((__m128i *)(outputPointer + (i + 1) * width + j + 1), G);
+            // _mm_storeu_si128((__m128i *)(outputPointer + (i + 1) * width + j + 1), G);
         }
     }
 
     // stop timer
-    t2 = getCurrentTimeInMicroseconds();
-    // calculate and print elapsed time in microseconds
-    elapsedTime = calculateElapsedTime(t1, t2);
-    cout << "Execution time for SIMD Sobel edge detection:" << endl;
-    cout << elapsedTime << " us" << endl;
+    // t2 = getCurrentTimeInMicroseconds();
+    // // calculate and print elapsed time in microseconds
+    // elapsedTime = calculateElapsedTime(t1, t2);
+    // cout << "Execution time for SIMD Sobel edge detection:" << endl;
+    // cout << elapsedTime << " us" << endl;
 
-    // crop image and remove added borders
-    outputImage = outputImage(cropSize);
+    // // crop image and remove added borders
+    // outputImage = outputImage(cropSize);
 
-    // Copy outputImage to resultImageSimd
-    resultImageSimd = outputImage.clone();
+    // // Copy outputImage to resultImageSimd
+    // resultImageSimd = outputImage.clone();
 }
 
 void SobelOpenCV(Rect cropSize, Mat inputImage)
